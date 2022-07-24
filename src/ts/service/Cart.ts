fac3661a267540fe1e2c3426438045be9729f7ac
Annie Bauer <overheads165@gmail.com>
2022-07-24 23:39:18 +0300

    functions

--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -10,4 +10,16 @@ export default class Cart {
     get items(): Buyable[] {
         return [...this._items]; 
     }
+
+    sum(): number{
+        return this._items.reduce((acc, item) => acc + item.price, 0);
+    }
+
+    sumWithDiscount(discount: number): number {
+        return this.sum() * (1 - discount);
+    }
+
+    delete(id: number): void {
+        this._items = this._items.filter(item => item.id !== id);
+    }
 }
